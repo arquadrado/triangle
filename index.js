0aac4208c881c3f209a5766b202d77169d78a9e6
António Quadrado <antoniorquadrado@gmail.com>
2017-03-13 00:43:26 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,73 +3,87 @@
 console.log('firing up')
 
 const sortAlpha = (string) => {
-    return string.split('').sort().join('');
+  return string.split('').sort().join('');
 }
 
 const multiplyPolynomials = (polyA, polyB) => {
 
-    let result = []
+  let result = []
 
-    polyA.forEach((polyATerm) => {
-        polyB.forEach((polyBTerm) => {
-            result.push(sumPolynomialTerms(polyATerm, polyBTerm))
-        })
+  polyA.forEach((polyATerm) => {
+    polyB.forEach((polyBTerm) => {
+      result.push(multiplyPolynomialTerms(polyATerm, polyBTerm))
     })
+  })
 
-    result = result.reduce((processedResult, term) => {
-        const coeficient = term.match(/\d/g) === null ? 1 : Number(term.match(/\d/g).join(''))
-        const rawTerm = term.match(/[a-zA-Z]+/g).join('')
+  result = result.reduce((processedResult, term) => {
+    const coeficient = term.match(/\d+/g) === null ? 1 : Number(term.match(/\d+/g).join(''))
+    const rawTerm = term.match(/[a-zA-Z]+/g).join('')
 
-        if (processedResult.hasOwnProperty(rawTerm)) {
-            processedResult[rawTerm].coeficient += coeficient
-            return processedResult
-        }
+    if (processedResult.hasOwnProperty(rawTerm)) {
+      processedResult[rawTerm].coeficient += coeficient
+      return processedResult
+    }
 
-        processedResult[rawTerm] = {
-            coeficient: coeficient
-        }
-        return processedResult
+    processedResult[rawTerm] = {
+      coeficient: coeficient
+    }
 
-    }, {})
+    return processedResult
 
-    return result
+  }, {})
+
+  return result
 }
 
 const formatPolynomial = (polynomial) => {
-    const poly = []
-    for (let term in polynomial) {
-        const coeficient = polynomial[term].coeficient === 1 ? '' : polynomial[term].coeficient
-        poly.push(coeficient + term)
-    }
+  const poly = []
+  for (let term in polynomial) {
+    const coeficient = polynomial[term].coeficient === 1 ? '' : polynomial[term].coeficient
+    poly.push(coeficient + term)
+  }
 
-    return poly
+  return poly
 }
 
 const binomialPower = (binomial, power) => {
-    if ( power > -1) {
+  if (power > -1) {
 
-        let compositePoly = binomial
-        let result = [[1]]
+    let compositePoly = binomial
+    let result = []
+    result.push(['1'])
 
-        for (let i = power; i > 0; i--) {
-            compositePoly = formatPolynomial(multiplyPolynomials(binomial, compositePoly))
-            result.push(compositePoly)
-        }
-
-        return result
-
+    if (power >= 1) {
+      result.push(binomial)
     }
 
-    return -1
+    if (power > 1) {
+      for (let i = power - 1; i > 0; i--) {
+        compositePoly = formatPolynomial(multiplyPolynomials(binomial, compositePoly))
+        result.push(compositePoly)
+      }
+    }
+
+    return result
+
+  }
+
+  return -1
 }
 
-const sumPolynomialTerms = (termA, termB) => {
-    const coeficientA = termA.match(/\d/g) === null ? '' : Number(termA.match(/\d/g).join(''))
-    const coeficientB = termB.match(/\d/g) === null ? '' : Number(termB.match(/\d/g).join(''))
-    const rawTermA = termA.match(/[a-zA-Z]+/g).join('')
-    const rawTermB = termB.match(/[a-zA-Z]+/g).join('')
+const multiplyPolynomialTerms = (termA, termB) => {
+  console.log(termA, termB)
+  const coeficientA = termA.match(/\d+/g) === null ? 1 : Number(termA.match(/\d+/g).join(''))
+  const coeficientB = termB.match(/\d+/g) === null ? 1 : Number(termB.match(/\d+/g).join(''))
 
-    return sortAlpha((coeficientA + coeficientB) + rawTermA + rawTermB)
+  const rawTermA = termA.match(/[a-zA-Z]+/g).join('')
+  const rawTermB = termB.match(/[a-zA-Z]+/g).join('')
+
+  return (coeficientA * coeficientB) + sortAlpha(rawTermA + rawTermB)
+}
+
+const extractBinomialCoeficients = (term) => {
+  return term.match(/\d+/g) === null ? 1 : Number(term.match(/\d+/g).join(''))
 }
 
 
@@ -77,10 +91,37 @@ const sumPolynomialTerms = (termA, termB) => {
 const binomial = ['x', 'y']
 const trinomial = ['xx', '2xy', 'yy']
 const tetranomial = ['xxx', '3xxy', '3xyy', 'yyy']
+const fivePowerPoly = ['xxxxx', '5xxxxy', '10xxxyy', '10xxyyy', '5xyyyy', 'yyyyy']
 
 //console.log(formatPolynomial(multiplyPolynomials(binomial, trinomial)))
 //console.log(formatPolynomial(multiplyPolynomials(binomial, tetranomial)))
 
-console.log(binomialPower(trinomial, 1))
+//console.log(binomialPower(binomial, 6))
+//console.log(multiplyPolynomials(binomial, fivePowerPoly))
 //console.log(sumPolynomialTerms('2xy', '2xxy'));
+
+
+
+const buildTriangle = () => {
+	const lines = document.getElementById('lines').value
+  if (lines > -1) {
+    const poweredBinomial = binomialPower(binomial, lines)
+    const $triangle = document.getElementById('triangle')
+    poweredBinomial.forEach((line) => {
+      const triangleLine = document.createElement('li')
+      triangleLine.className = 'line'
+      line.forEach((element) => {
+        const elementContent = document.createElement('span')
+        elementContent.className = 'element'
+        elementContent.appendChild(document.createTextNode(extractBinomialCoeficients(element)))
+
+        triangleLine.appendChild(elementContent)
+      })
+      $triangle.appendChild(triangleLine)
+    })
+  }
+
+}
+
+document.getElementById('build').addEventListener('click', buildTriangle)
 
